d6e069bccbdb55d8c75635cab081a9458f6eea06
Alina Stirbu <alina.stirbu@webbeds.com>
2019-11-21 16:23:19 +0200

    App3 console app

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string initial;
+            int[] occurences = new int[char.MaxValue];
+            Console.WriteLine("Introduce a string:");
+            initial = Console.ReadLine();
+            if(initial.Length > 0)
+            {
+                foreach (char c in initial)
+                {
+                    occurences[(int)c]++;
+                }
+                for(int i = 0; i < occurences.Length-1; i++)
+                {
+                    if(occurences[i]>=1)
+                    {
+                        Console.WriteLine((char)i + " occurs for " + occurences[i] + " times");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("String should have length > 0");
+            }
+
+        }
+    }
+}
