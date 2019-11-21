e04cb274b327924cddc347c5c0a162d0b77f4f31
Alina Stirbu <alina.stirbu@webbeds.com>
2019-11-21 12:00:48 +0200

    First console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String initial = "";
+            String final = "";
+            Console.WriteLine("Introduce a string:");
+            initial = Console.ReadLine();
+            if(initial.Length > 0)
+            {
+                foreach (char c in initial)
+                {
+                    if (final.IndexOf(c) == -1)
+                        final = final + c;
+                }
+                Console.WriteLine($" String without duplicates is {final}");
+            }
+            else
+            {
+                Console.WriteLine("String has length = 0.");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
