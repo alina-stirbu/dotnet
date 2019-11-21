21a8bc6c3b08dea2ad6c70893d1d5480bb2de86e
Alina Stirbu <alina.stirbu@webbeds.com>
2019-11-21 16:26:46 +0200

    App6 console app

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce list content!");
+            string text = Console.ReadLine();
+            if (text.Length > 0)
+            {
+                //last occurence of space
+                int lastSpace = text.LastIndexOf(' ');
+                if (lastSpace > -1)
+                {
+                    Console.WriteLine(text.Length - (lastSpace+1));
+                }
+                else
+                {
+                    Console.WriteLine('0');
+                }
+            }
+            else
+            {
+                Console.WriteLine("Introduce a text with length > 0 ");
+            }
+        }
+    }
+}
