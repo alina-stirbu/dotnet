98fa7116d1e8e0f73f97397528542f706e233ce9
Alina Stirbu <alina.stirbu@webbeds.com>
2019-12-05 01:50:31 +0200

    ex no 3

---
+++ ConsoleAppRangeException/InvalidRangeException.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppRangeException
+{
+    [Serializable]
+    public class InvalidRangeException<T>:Exception
+    {
+        private static readonly string DefaultMessage = "Invalid range";
+        public T StartRangeInterval { get; set; }
+        public T StopRangeInterval { get; set; }
+
+        /// <summary>
+        /// first 3 contructors are required
+        /// </summary>
+        public InvalidRangeException() : base(DefaultMessage) { }
+        public InvalidRangeException(string message) : base(message) { }
+        public InvalidRangeException(string message, System.Exception innerException)
+        : base(message, innerException) { }
+
+        /// <summary>
+        /// constructor of custom exceptio that set range ends
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="valueStart"></param>
+        /// <param name="valueStop"></param>
+        public InvalidRangeException(string message, T valueStart, T valueStop)
+        : base(message)
+        {
+            StartRangeInterval = valueStart;
+            StopRangeInterval = valueStop;
+        }
+        /// <summary>
+        /// other constructor that set ranges as innerexception
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="valueStart"></param>
+        /// <param name="valueStop"></param>
+        /// <param name="innerException"></param>
+        public InvalidRangeException(string message, T valueStart, T valueStop, System.Exception innerException)
+        : base(message, innerException)
+        {
+            StartRangeInterval = valueStart;
+            StopRangeInterval = valueStop;
+        }
+
+        protected InvalidRangeException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ ConsoleAppRangeException/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppRangeException
+{
+    class Program
+    {
+        public static readonly int startInt = 0;
+        public static readonly int stopInt = 100;
+        public static readonly DateTime startDate = new DateTime(1980, 1, 1);
+        public static readonly DateTime stopDate = new DateTime(2013, 12, 31);
+
+        static void Main(string[] args)
+        {
+            List<int> intRange = new List<int> { 1, 2, 100, 5, -1};
+            List<DateTime> dateRange = new List<DateTime> { new DateTime(1980, 1, 2), new DateTime(1999, 03, 05), new DateTime(2020, 4, 5) };
+            
+            try
+            {
+                foreach (var elem in intRange)
+                {
+                    if (!IsInIntRange(elem))
+                    {
+                        throw new InvalidRangeException<int>($"Elem is not in range: {elem}",startInt, stopInt);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            //used for date time
+            try
+            {
+                foreach (var elem in dateRange)
+                {
+                    if (!IsInDateRange(elem))
+                    {
+                        throw new InvalidRangeException<DateTime>($"Elem is not in range: {elem}", startDate, stopDate);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static bool IsInIntRange(int element)
+        {
+            if (element >= startInt && element <= stopInt)
+                return true;
+            return false;
+        }
+
+        public static bool IsInDateRange(DateTime element)
+        {
+            if (element >= startDate && element <= stopDate)
+                return true;
+            return false;
+        }
+    }
+}
