5bfcfefa29a8e097f4f9d559b3de126993e1b778
Alina Stirbu <alina.stirbu@webbeds.com>
2019-12-05 01:47:11 +0200

    ex no 2

---
+++ ConsoleAppIEnumerableExtension/Extensions.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppIEnumerableExtension
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// used to calculate sum of colection elemens
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="numeralCollection"></param>
+        /// <returns></returns>
+        public static T Sum<T>(this IEnumerable<T> numeralCollection) where T : struct
+        {
+            T result = numeralCollection.FirstOrDefault();
+            if(!result.Equals(default(T)))
+            {
+                foreach(var item in numeralCollection.Skip(1))
+                {
+                    result += (dynamic)item;
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// used to calculate the product of every element in colection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="numeralCollection"></param>
+        /// <returns></returns>
+        public static T Product<T>(this IEnumerable<T> numeralCollection) where T : struct
+        {
+            T result = numeralCollection.FirstOrDefault();
+            if (!result.Equals(default(T)))
+            {
+                foreach (var item in numeralCollection.Skip(1))
+                {
+                    result *= (dynamic)item;
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// used to calculate the minimum value from all elements
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="numeralCollection"></param>
+        /// <returns></returns>
+        public static T Min<T>(this IEnumerable<T> numeralCollection) where T : struct
+        {
+            T result = numeralCollection.FirstOrDefault();
+            if (!result.Equals(default(T)))
+            {
+                foreach (var item in numeralCollection.Skip(1))
+                {
+                    if(result > (dynamic)item )
+                    {
+                        result = item;
+                    }
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// used to calculate max value from all elements in colection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="numeralCollection"></param>
+        /// <returns></returns>
+        public static T Max<T>(this IEnumerable<T> numeralCollection) where T : struct
+        {
+            T result = numeralCollection.FirstOrDefault();
+            if (!result.Equals(default(T)))
+            {
+                foreach (var item in numeralCollection.Skip(1))
+                {
+                    if(result < (dynamic)item)
+                    {
+                        result = item;
+                    }
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// used to calculate the average value for elements of colection
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="numeralCollection"></param>
+        /// <returns></returns>
+        public static T Average<T>(this IEnumerable<T> numeralCollection) where T : struct
+        {
+            T result = numeralCollection.FirstOrDefault();
+            if (!result.Equals(default(T)))
+            {
+                int numberOfElements = 1;
+                T sumOfElements = result;
+                foreach (var item in numeralCollection.Skip(1))
+                {
+                    sumOfElements += (dynamic)item;
+                    numberOfElements++;
+                }
+                result = (dynamic)sumOfElements / numberOfElements;
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleAppIEnumerableExtension/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppIEnumerableExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 1, 7, 9, 11 };
+
+            int sumIntList = intList.Sum<int>();
+            int maxIntList = intList.Max<int>();
+            int minIntList = intList.Min();
+
+            Console.WriteLine("Sum for intList is {0}",sumIntList);
+            Console.WriteLine("Min for intList is {0}", minIntList);
+            Console.WriteLine("Max for intList is {0}", maxIntList);
+            Console.WriteLine("Product for intList is {0}", intList.Product());
+            Console.WriteLine("Average for intList is {0}", intList.Average());
+
+            //empty list of decimals
+            List<decimal> decimalList = new List<decimal> {};
+            Console.WriteLine("Sum for decimalList is {0}", decimalList.Sum());
+            Console.WriteLine("Max for decimalList is {0}", decimalList.Max());
+            Console.WriteLine("Average for decimalList is {0}", decimalList.Average());
+
+            List<double> doubleList = new List<double> {11.3,4.5,8.0,12};
+
+            double sumDoubleList = doubleList.Sum<double>();
+            double maxDoubleList = doubleList.Max<double>();
+            double minDoubleList = doubleList.Min();
+
+            Console.WriteLine("Sum for doubleList is {0}", sumDoubleList);
+            Console.WriteLine("Min for doubleList is {0}", maxDoubleList);
+            Console.WriteLine("Max for doubleList is {0}", minDoubleList);
+            Console.WriteLine("Product for doubleList is {0}", doubleList.Product());
+            Console.WriteLine("Average for doubleList is {0}", doubleList.Average());
+
+        }
+    }
+}
