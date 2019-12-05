fba96e19de7e685b8365ed0ac289ba2d7bf44b3a
Alina Stirbu <alina.stirbu@webbeds.com>
2019-12-05 13:20:01 +0200

    ex no. 1

---
+++ ConsoleAppTimer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppTimer
+{
+    public delegate void TimerDelegate(string messageText);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimerDelegate d = new TimerDelegate(DisplayMessage);
+            Timer1 timer = new Timer1(100);
+            timer.MessageArrived += d;
+        }
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ConsoleAppTimer/Timer1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using static ConsoleAppTimer.Program;
+
+namespace ConsoleAppTimer
+{
+    public class Timer1
+    {
+        public event TimerDelegate MessageArrived;
+        private Timer CurrentTimer;
+
+        public Timer1(int t)
+        {
+            CurrentTimer = new Timer(t);
+            CurrentTimer.Elapsed += new ElapsedEventHandler(CustomMethod);
+            CurrentTimer.Start();
+        }
+
+        private void CustomMethod(object source, ElapsedEventArgs e)
+        {
+            if(MessageArrived == null)
+            {
+                MessageArrived("Event from Timer1");
+            }
+        }
+    }
+}
