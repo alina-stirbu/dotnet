6d77cb96ae3488345da9a6418614f67cb138d927
Alina Stirbu <alina.stirbu@webbeds.com>
2019-12-11 11:52:04 +0200

    ex 2

---
+++ ConsoleAppCountWords/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCountWords
+{
+    class Program
+    {
+        private const string Path =
+                "C:\\Users\\Alina\\source\\repos\\ConsoleAppCountWords\\files";
+        static void Main(string[] args)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            List<WordClass> wList = new List<WordClass>();
+            List<Task> tasks = new List<Task>();
+
+            try
+            {
+                //get the number of files from path
+                string[] fileEntries = Directory.GetFiles(Path, "*", SearchOption.TopDirectoryOnly);
+                if (fileEntries.Length > 0)
+                {
+                    foreach (string fileName in fileEntries)
+                    {
+                        var word = new WordClass();
+                        wList.Add(word);
+                        Action someAction = () => { word.ReadFile(fileName); };
+                        var t = new Task(someAction);
+                        tasks.Add(t);
+                        t.Start();
+                    }
+
+                    for (int i= 0;i< tasks.Count;i++)
+                    {
+                        tasks[i].Wait();
+                        wList[i].GetDistinctWordCount();
+                        wList[i].GroupWords();
+
+                    }
+                    stopwatch.Stop();
+
+                    Console.WriteLine($"Elapsed time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+                    
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            
+        }
+    }
+}
---
+++ ConsoleAppCountWords/WordClass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppCountWords
+{
+    
+    /// <summary>
+    /// class that read words and process them
+    /// </summary>
+    class WordClass
+    {
+        public List<string> wordList { get; private set; }
+        public Dictionary<string, string> wordDictionary { get; private set; }
+        public WordClass()
+        {
+            wordList = new List<string>();
+            wordDictionary = new Dictionary<string, string>();
+        }
+        /// <summary>
+        /// 
+        /// read file line by line and put in the list of stings
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void ReadFile(string filePath)
+        {
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                StreamReader sr = new StreamReader(fs);
+                string line = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    wordList.Add(line);
+                }
+
+                sr.Close();
+                fs.Close();
+
+                Console.WriteLine("Read {0} words from file {1}",wordList.Count, filePath);
+            }
+        }
+        /// <summary>
+        /// get number of words from a file
+        /// </summary>
+        /// <returns></returns>
+        public int GetWordCount()
+        {
+            return wordList.Count;
+        }
+        /// <summary>
+        /// get number of distinct words
+        /// </summary>
+        /// <returns></returns>
+        public int GetDistinctWordCount()
+        {
+            return wordList.Distinct().Count();
+        }
+        /// <summary>
+        /// find if a given word existis in the list of words
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public bool FindWord(string word)
+        {
+            return wordList.Contains(word);
+        }
+        /// <summary>
+        /// group words by length
+        /// </summary>
+        public void GroupWords()
+        {
+            var x = wordList.GroupBy(w =>
+            {
+                if (w.Length >0 && w.Length <= 5) return "xs"; if (w.Length > 5 && w.Length <= 10) return "s";
+                if (w.Length > 10 && w.Length <= 15) return "m"; return "l";
+            });
+        }
+
+        
+    }
+}
