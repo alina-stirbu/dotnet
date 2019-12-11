641fd032e6f3ea617cf812958296c378e70b5cd9
Alina Stirbu <alina.stirbu@webbeds.com>
2019-12-11 11:46:33 +0200

    ex 1

---
+++ ConsoleAppFindDivisors/Divisors.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppFindDivisors
+{
+    /// <summary>
+    /// class used to store the list of numbers and their divisors
+    /// </summary>
+    class Divisors
+    {
+        private readonly int nrOfElementsToProcess;
+        private readonly int startIndex;
+        public Dictionary<int,int> DivisorsList { get; private set; }
+
+        public Divisors(int startIndex, int nrOfElementsToProcess)
+        {
+            DivisorsList = new Dictionary<int, int>();
+            this.startIndex = startIndex;
+            this.nrOfElementsToProcess = nrOfElementsToProcess;
+            for(int i = startIndex; i< nrOfElementsToProcess; i++)
+            {
+                DivisorsList.Add(i,0);
+            }
+        }
+        /// <summary>
+        /// method used to calc divisors for an interval
+        /// </summary>
+        public void CalculateDivisors()
+        {
+            var to = this.startIndex + this.nrOfElementsToProcess;
+
+            for (var i = this.startIndex; i < to; i++)
+            {
+                DivisorsList[i] = this.ProcessDivisor(i);
+            }
+            Console.WriteLine("In task {0} - {1}", this.startIndex, to);
+        }
+
+        /// <summary>
+        /// method used to calc divisor for a specific number
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        private int ProcessDivisor(int i)
+        {
+            int number = 0;
+            for (int k = 1; k <= Math.Sqrt(i); k++)
+            {
+                if(i%k == 0)
+                {
+                    if (i / k == k)
+                        number++;
+                    else
+                        number += 2;
+                }
+            }
+            return number;
+        }
+    }
+}
---
+++ ConsoleAppFindDivisors/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFindDivisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arraySize = 10000;
+            
+            var stopwatch = Stopwatch.StartNew();
+            //no of cores
+            int nr = Environment.ProcessorCount;
+            Console.WriteLine($"Cores: {nr}");
+            List<Divisors> divList = new List<Divisors>();
+            List<Task> tasks = new List<Task>();
+            //resulting dictionary after processing
+            Dictionary<int, int> mergedDictionary = new Dictionary<int, int>();
+            int batchSize = arraySize / nr;
+            for (int i = 0; i < nr+1; i++)
+            {
+                int startIndex = i * batchSize;
+                int batchSizeAdjusted = batchSize;
+                if ((startIndex + batchSizeAdjusted) > arraySize)
+                    batchSizeAdjusted = arraySize - startIndex;
+                var divisor = new Divisors(startIndex, batchSizeAdjusted);
+                divList.Add(divisor);
+                var t = new Task(divisor.CalculateDivisors);
+                tasks.Add(t);
+                t.Start();
+            }
+            for (int i = 0; i < nr+1; i++)
+            {
+                tasks[i].Wait();
+                mergedDictionary = mergedDictionary.Union(divList[i].DivisorsList).ToDictionary(d => d.Key, d => d.Value);
+            }
+            var maxDivisor = mergedDictionary.FirstOrDefault(p => p.Value == mergedDictionary.Max(p => p.Value));
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Elapsed time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+            Console.WriteLine("Number wit max divisors is {0} and has {1} divisors: ", maxDivisor.Key, maxDivisor.Value);
+
+        }
+    }
+}
