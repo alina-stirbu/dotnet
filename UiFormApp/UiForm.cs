b4216c1dcbd6c4ce7709b424e2281e6d4e16c9bf
Alina Stirbu <alina.stirbu@webbeds.com>
2019-12-12 04:54:49 +0200

    ex 3

---
+++ UiFormApp/UiForm.cs
@@ -0,0 +1,47 @@
+﻿namespace UiFormApp
+{
+    using System.Diagnostics;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class UiForm : Form
+    {
+        public UiForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void DownloadBtnLeft_Click(object sender, System.EventArgs e)
+        {
+            var url = this.urlTextBoxLeft.Text;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string source = await DownloadStringMethod(url);
+
+            this.contentTxbLeft.Text = source;
+            this.logLabelLeft.Text = $@"Downloaded in {stopwatch.ElapsedMilliseconds} ms";
+        }
+
+        private async void DownloadBtnRight_Click(object sender, System.EventArgs e)
+        {
+            var url = this.urlTextBoxRight.Text;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string source = await DownloadStringMethod(url);
+
+            this.contentTxbRight.Text = source;
+            this.logLabelRight.Text = $@"Downloaded in {stopwatch.ElapsedMilliseconds} ms";
+        }
+
+        private async Task<string> DownloadStringMethod(string url)
+        {
+            string result = await (new WebClient().DownloadStringTaskAsync(url));
+            return result;
+        }
+    }
+}
