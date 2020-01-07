723a2ec3e3733e685798c1d792817723ac538cf1
Alina Stirbu <alina.stirbu@webbeds.com>
2020-01-07 14:25:07 +0200

    ex 5-3

---
+++ SOLID.DependencyInversion/2.2.WorkerAfter/Manager.cs
@@ -0,0 +1,16 @@
+﻿namespace DependencyInversionWorkerAfter
+{
+    public class Manager
+    {
+        IWorker worker;
+
+        public Manager(IWorker worker)
+        {
+            this.worker = worker;
+        }
+        public void Manage()
+        {
+            this.worker.Work();
+        }
+    }
+}
